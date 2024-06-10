29c1d77335e0c13e89dd4ac02e33bb94e5d1bd19
Jean 'clark' EYMERT <jean@akiros.io>
2024-06-10 12:00:55 +0200

    add some outputs

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,16 @@ output "iam_role_step_function" {
   value = aws_iam_role.step_function.arn
 }
 
+output "state_machine_arn" {
+  value = aws_sfn_state_machine.refresh_env.arn
+}
+
 output "state_machine_name" {
   value = aws_sfn_state_machine.refresh_env.name
+}
+
+output "step_function_json_files_local_path" {
+  value = local_file.step_function_json_input.filename
 }
 
 output "step_function_json_files" {
